340162c7fe13f787f89cbd7fc84b51e81a4e835e
MohanrajPM <mohanrajpmy@gmail.com>
2020-01-16 22:52:46 +0530

    multi vendor tortoise

---
+++ app/src/main/java/com/agri/multivendor/SellersLogin.java
@@ -0,0 +1,14 @@
+package com.agri.multivendor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SellersLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sellers_login);
+    }
+}
