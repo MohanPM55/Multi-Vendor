6cfc117b20164e81e158b4d56148f5210a03bc86
Srinivasan8863 <shreersv333@gmail.com>
2020-01-17 22:27:26 +0530

    A fresh project is prepared

--- app/src/main/java/com/agri/multivendor/MainActivity.java
+++ app/src/main/java/com/agri/multivendor/MainActivity.java
@@ -11,19 +11,11 @@ import android.os.Handler;
 import com.agri.multivendor.R;
 
 public class MainActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 4000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent homeIntent = new Intent(MainActivity.this,HomeActivity.class);
-                startActivity(homeIntent);
-                finish();
-            }
-        },SPLASH_TIME_OUT);
 
     }
 }
