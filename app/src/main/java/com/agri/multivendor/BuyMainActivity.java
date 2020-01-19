bbf1a4162350ab63a012235aa259e8c30b7278b5
Srinivasan8863 <shreersv333@gmail.com>
2020-01-19 23:11:32 +0530

    Modified MainActivity.java, AndroidManifest.xml, activity_main.xml and Added BuyMainActivity.java, main/res/layout/activity_buy_main.xml

---
+++ app/src/main/java/com/agri/multivendor/BuyMainActivity.java
@@ -0,0 +1,14 @@
+package com.agri.multivendor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BuyMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buy_main);
+    }
+}
--- app/src/main/java/com/agri/multivendor/MainActivity.java
+++ app/src/main/java/com/agri/multivendor/MainActivity.java
@@ -5,17 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-
-
-import com.agri.multivendor.R;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buyButton = findViewById(R.id.btnBuy);
+        final Intent intent = new Intent(this, BuyMainActivity.class);
+        buyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
+
+
+
 
     }
 }
